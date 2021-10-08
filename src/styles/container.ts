96d4b96be0f301d1c6c287eb6f7ed947f49b1a77
Filipe <filipeseventeen1@gmail.com>
2021-10-08 20:05:23 -0300

    feat: done the header responsive

--- src/styles/container.ts
+++ src/styles/container.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components'
 
 export const GlobalContainer = styled.div`
-  width: 120rem;
+  max-width: 120rem;
   margin: 0 auto;
-  /* padding: 0 25rem;
-  border: 1px solid blue; */
 `
