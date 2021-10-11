4e2b1ba6f8ce46436faf1fb7b75b74fe7fa97e78
Filipe <filipeseventeen1@gmail.com>
2021-10-10 21:53:27 -0300

    feat: done customers served responsive

--- src/components/layout/CustomersServed/style.ts
+++ src/components/layout/CustomersServed/style.ts
@@ -8,6 +8,10 @@ export const ErasedTitle = styled.h1`
   font-size: 2.2rem;
   color: rgba(255, 255, 255, 0.4);
   font-weight: 400;
+
+  @media (max-width: 1030px) {
+    text-align: center;
+  }
 `
 
 export const Grid = styled.div`
@@ -15,6 +19,19 @@ export const Grid = styled.div`
   grid-template-columns: repeat(7, 1fr);
   grid-template-rows: 1fr;
   margin-top: 4rem;
+
+  @media (max-width: 1030px) {
+    grid-template-columns: repeat(3, 127px);
+    grid-template-rows: repeat(3, 1fr);
+    row-gap: 4rem;
+    justify-content: center;
+    column-gap: 4rem;
+  }
+
+  @media (max-width: 640px) {
+    grid-template-columns: repeat(2, 127px);
+    grid-template-rows: repeat(4, 1fr);
+  }
 `
 
 export const Image = styled.img`
